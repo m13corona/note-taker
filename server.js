68d4aa3124201acb32eff8b0fb20e8eeead8aa0e
m13corona <mcorona1308@gmail.com>
2024-03-11 22:44:03 -0600

    installed necessary programs

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const express = require('express');
+const fs = require('fs')
+
 
 const app = express();
 const PORT = 3001;
 
 app.get('/', (req, res) => {
 //route logic
-
+    res.sendFile('./index.html')
 });
-
 
 app.post('/notes', (req, res) => {
 //route logic
 
 })
-
 
 app.listen(PORT, () =>
     console.log(`Listening for requests on port ${PORT}! `)
