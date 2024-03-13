10c87fd23038ed8b6880cc1bf410175c1a79e1d7
m13corona <mcorona1308@gmail.com>
2024-03-12 21:34:28 -0600

    added code for GET request

--- server.js
+++ server.js
@@ -6,24 +6,35 @@ const app = express();
 const PORT = 3001;
 
 
-app.get('/', (req, res) => {
+app.get('/notes', (req, res) => {
 //route logic
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
+});
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
 
 
-app.get('/notes', (req, res) => {
+
+
+
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
 
-app.post('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+//app.post('/notes', (req, res) => {
+   // res.sendFile(path.join(__dirname, '/public/notes.html'))
 //route logic
 
-})
+//})
 
-app.listen(PORT, () =>
-    console.log(`Listening for requests on port ${PORT}! `)
+//app.listen(PORT, () =>
+ //   console.log(`Listening at http://localhost:${PORT} `)
 
-);
+//);
