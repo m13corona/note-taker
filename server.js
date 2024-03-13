bb9806c12a59ac4fc558a5868b0c686f56511ddc
m13corona <mcorona1308@gmail.com>
2024-03-13 17:18:44 -0600

    add code for app to retrieve list

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ const path = require('path');
 const app = express();
 const PORT = 3001;
 
+//const { clog } = require('./middleware/clog');
+//const api = require('./routes/index.js');
+
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+//app.use(express.urlencoded({ extended: true }));
+//app.use('/api', api);
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
