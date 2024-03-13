ed854e574e4a1b02a933c47a50c258520df3d166
m13corona <mcorona1308@gmail.com>
2024-03-12 20:54:52 -0600

    add code on server.js and install more packages

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const fs = require('fs')
 
-
+const path = require('path');
 const app = express();
 const PORT = 3001;
 
+
 app.get('/', (req, res) => {
 //route logic
-    res.sendFile('./index.html')
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
+
 app.post('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 //route logic
 
 })
